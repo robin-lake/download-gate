1a38a754750fa0e12dfc4ef41ec1d6cc1c92435c
robin-lake <robinhlake@gmail.com>
2026-03-07 18:45:50 -0800

    bandcamp popup

--- frontend/src/pages/NewDownloadGate/GateStep.tsx
+++ frontend/src/pages/NewDownloadGate/GateStep.tsx
@@ -4,6 +4,9 @@ import { cn } from "@/lib/utils";
 import SpotifyStepConfigPopup, {
   type SpotifyStepConfig,
 } from "./SpotifyStepConfigPopup";
+import BandcampStepConfigPopup, {
+  type BandcampStepConfig,
+} from "./BandcampStepConfigPopup";
 
 /** One gate step in the form (selection order = step_order). */
 export interface GateStepFormItem {
@@ -27,7 +30,7 @@ export default function GateStep(props: GateStepProps) {
   const isSelected = Boolean(existingStep);
 
   const handleClick = () => {
-    if (service_type === "spotify") {
+    if (service_type === "spotify" || service_type === "bandcamp") {
       setConfigPopupOpen(true);
     } else {
       // Simple add/remove for other integration types
@@ -64,7 +67,31 @@ export default function GateStep(props: GateStepProps) {
     setConfigPopupOpen(false);
   };
 
+  const handleBandcampSave = (config: BandcampStepConfig, is_skippable: boolean) => {
+    const others = value.filter((s) => s.service_type !== service_type);
+    const { follow_profile_enabled, profile_urls } = config;
+    onChange([
+      ...others,
+      {
+        service_type,
+        is_skippable,
+        config: { follow_profile_enabled, profile_urls } as unknown as Record<string, unknown>,
+      },
+    ]);
+    setConfigPopupOpen(false);
+  };
+
+  const handleBandcampCancel = () => {
+    setConfigPopupOpen(false);
+  };
+
+  const handleBandcampDelete = () => {
+    onChange(value.filter((s) => s.service_type !== service_type));
+    setConfigPopupOpen(false);
+  };
+
   const spotifyConfig = existingStep?.config as Partial<SpotifyStepConfig> | undefined;
+  const bandcampConfig = existingStep?.config as Partial<BandcampStepConfig> | undefined;
 
   return (
     <>
@@ -88,6 +115,22 @@ export default function GateStep(props: GateStepProps) {
           onSave={handleSpotifySave}
           onCancel={handleSpotifyCancel}
           onDelete={isSelected ? handleSpotifyDelete : undefined}
+        />
+      )}
+      {service_type === "bandcamp" && (
+        <BandcampStepConfigPopup
+          open={configPopupOpen}
+          stepNumber={
+            existingStep
+              ? value.findIndex((s) => s.service_type === "bandcamp") + 1
+              : value.length + 1
+          }
+          initialConfig={bandcampConfig}
+          initialIsSkippable={existingStep?.is_skippable ?? false}
+          isEditing={isSelected}
+          onSave={handleBandcampSave}
+          onCancel={handleBandcampCancel}
+          onDelete={isSelected ? handleBandcampDelete : undefined}
         />
       )}
     </>
